50409bfd0c9cbbd1b63a7ae8a2e48603edd4b090
sergiobni <bni.sergio@gmail.com>
2024-03-30 10:34:51 +0100

    updating app settings

---
+++ src/features/settings/useUpdateSettings.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import { updateSetting as UpdateSettingApi } from '../../services/apiSettings';
+
+export function useUpdateSetting() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
+    mutationFn: UpdateSettingApi,
+    onSuccess: () => {
+      toast.success('Setting edited!');
+      queryClient.invalidateQueries({
+        queryKey: ['settings'],
+      });
+    },
+    onError: err => toast.error(err.message),
+  });
+
+  return { isUpdating, updateSetting };
+}
